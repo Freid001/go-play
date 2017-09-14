5a71fb86a729fe73c927b4618fb92f0512968f92
freid <freid@mediamath.com>
2017-09-14 16:25:42 +0100

    refactor

---
+++ hello.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+const HELLO = "hello"
+
+func hello(name string) {
+	fmt.Println("Hello " + name + "!")
+}
--- main.go
+++ main.go
@@ -8,20 +8,25 @@ import (
 
 func main() {
 	//Commands
+	helloCommand := flag.NewFlagSet(HELLO, flag.ExitOnError)
 	fizzBuzzCommand := flag.NewFlagSet(FIZZBUZZ, flag.ExitOnError)
 
 	//Options
+	helloNameString := helloCommand.String("name", "", "Your name. (Required)")
 	fizzBuzzNumbersString := fizzBuzzCommand.String("numbers", "", "Comma separated string of number. (Required)")
 
 	if len(os.Args) < 2 {
 		flag.PrintDefaults()
 
 		fmt.Println("Commands:")
+		fmt.Println("   "+HELLO+"\t output your name.")
 		fmt.Println("   "+FIZZBUZZ+"\t for multiples of three print 'Fizz' and for multiples of five print 'Buzz'.")
 		return
 	}
 
 	switch os.Args[1] {
+	case HELLO:
+		helloCommand.Parse(os.Args[2:])
 	case FIZZBUZZ:
 		fizzBuzzCommand.Parse(os.Args[2:])
 	default:
@@ -29,6 +34,15 @@ func main() {
 		return
 	}
 
+	if helloCommand.Parsed() {
+		if *helloNameString == "" {
+			helloCommand.PrintDefaults()
+		}else {
+			hello(*helloNameString)
+		}
+	}
+
+	//FizzBuzz
 	if fizzBuzzCommand.Parsed() {
 		if *fizzBuzzNumbersString == "" {
 			fizzBuzzCommand.PrintDefaults()
